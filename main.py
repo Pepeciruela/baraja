d5d6082f3825235bf3ae8b28d6cdd4da311f1be9
Angel <calvo.monzon.angel@gmail.com>
2020-11-11 00:24:52 +0100

    segunda actualizacion

--- main.py
+++ main.py
@@ -2,15 +2,14 @@
 
 palos = ["o", "c", "e", "b"]
 numeros = ["A", "1", "2", "3", "4", "5", "6", "7", "S", "C", "R"]
-baraja = []
 
 def crearBaraja():
+    baraja = []
     for palo in palos:
         for numero in numeros:
             baraja.append(numero + palo)
     return baraja
 
-crearBaraja()
 
 
 
