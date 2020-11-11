588d6ec0e80a8494175ec6f203f61740ea654988
Angel <calvo.monzon.angel@gmail.com>
2020-11-11 22:33:23 +0100

    barajear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#Fabricación de una baraja
+
+import random
 
 palos = ["o", "c", "e", "b"]
 numeros = ["A", "1", "2", "3", "4", "5", "6", "7", "S", "C", "R"]
@@ -10,6 +11,20 @@ def crearBaraja():
             baraja.append(numero + palo)
     return baraja
 
+def intercambio (a,b):
+    aux = a
+    a = b
+    b = aux
+    return a, b
+    
+def barajar(lista_de_naipes):
+    for i in range (len(lista_de_naipes):
+        nueva_pos = random.randrange(len(lista_de_naipes))
+        #Intercambio de cartas, técnica del vaso vacio
+        aux = lista_de_naipes[nueva_pos]
+        lista_de_naipes [nueva_pos] = lista_de_naipes [i]
+        lista_de_naipes [i] = aux
+    
 
 
 
