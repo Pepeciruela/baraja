e49e784f10253ba836e5e6ba59fffaa1ee2d6fbd
Angel <calvo.monzon.angel@gmail.com>
2020-11-12 22:23:32 +0100

    Final

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ palos = ["o", "c", "e", "b"]
 numeros = ["A", "1", "2", "3", "4", "5", "6", "7", "S", "C", "R"]
 
 miBaraja = barajaC.Baraja(palos, numeros)
-miBaraja.mazacote[2]
 
 print(miBaraja.mazacote)
 print(miBaraja.repartir(3,5))
